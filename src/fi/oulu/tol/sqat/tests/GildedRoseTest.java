d5d171ff6e81ddbc08d2dd88cc3b6abb157a573d
Benedikt Hein <benedikt.hein@stud.uni-regensburg.de>
2017-09-21 14:19:27 +0300

    Assignment Finished

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -82,7 +82,113 @@ public class GildedRoseTest {
 		
 	}
 	
+	@Test (expected=AssertionError.class)
+	public void testUpdateEndOfDay_ToHighQuality() {
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("AgedBrie", 3, 50));
+		
+		// Act
+		store.updateEndOfDay();
+				
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(51, itemBrie.getQuality());
+		
+	}
 	
+	@Test 
+	public void testUpdateEndOfDay_newNormalItem() {
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("ItemExample", 10, 15));
+		
+		// Act
+		store.updateEndOfDay();
+				
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemExample = items.get(0);
+		assertEquals(14, itemExample.getQuality());
+		
+	}
+	@Test
+	public void testUpdateEndOfDay_SellIn0() {
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("ItemExample0", 0, 15));
+		
+		// Act
+		store.updateEndOfDay();
+				
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemExample0 = items.get(0);
+		assertEquals(13, itemExample0.getQuality());
+		
+	}
+	
+	@Test 
+	public void testUpdateEndOfDay_QualityGetsNegativ() {
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("ItemExampleNegativ", 3, 0));
+		
+		// Act
+		store.updateEndOfDay();
+				
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemExampleNegativ = items.get(0);
+		assertEquals(0, itemExampleNegativ.getQuality());
+		
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_negativQuality2() {
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("ItemExample", 0, 1));
+		
+		// Act
+		store.updateEndOfDay();
+				
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemExample0 = items.get(0);
+		assertEquals(0, itemExample0.getQuality());
+		
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_BackstagePasses10() {
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Backstage passes, Aged Brie", 10, 10));
+		
+		// Act
+		store.updateEndOfDay();
+				
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemExample = items.get(0);
+		assertEquals(12, itemExample.getQuality());
+		
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_BackstagePasses5() {
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Backstage passes, Aged Brie", 5, 10));
+		
+		// Act
+		store.updateEndOfDay();
+				
+		// Assert
+		List<Item> items = store.getItems();
+		Item agedBri = items.get(0);
+		assertEquals(13, agedBri.getQuality());
+		
+	}
+	
+	
+	
+		
 	
 	
 	/*@Ignore
